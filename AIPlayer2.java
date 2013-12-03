9f6667c3a72f58b799fffe398b0933ff0a07b258
Bertie Wheen <wheen.b@gmail.com>
2013-12-03 15:57:28 +0000

    Update AIPlayer2.java

--- AIPlayer2.java
+++ AIPlayer2.java
@@ -1,10 +1,6 @@
 public class AIPlayer2 extends Player {
     private static final int NUM_HOUSES = Kalah.NUM_HOUSES;
 
-    public AIPlayer2(Player other) {
-        super(other);
-    }
-
     @Override
     public int getSowIndex(Board board, int lastMove) {
         return -1;
