5e40a687607dbe9f1c4b5f546c3d12f421a0f51d
Bertie Wheen <wheen.b@gmail.com>
2013-12-03 15:50:30 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,9 @@
 public abstract class Player {
+    protected Player other;
+    
+    public Player(Player other) {
+        this.other = other;
+    }
+
     public abstract int getSowIndex(Board board, int lastMove);
 }
