596a7e4c4fcb8a82956225c819f8e59603a01b89
J-shaw1 <joeramsfan@hotmail.co.uk>
2013-12-09 19:11:37 +0000

    Update AIPlayer2.java

--- AIPlayer2.java
+++ AIPlayer2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class AIPlayer2 extends Player {
     private static final int NUM_HOUSES = Kalah.NUM_HOUSES;
 
@@ -6,6 +9,26 @@ public class AIPlayer2 extends Player {
     private int index;
     private Board currentBoard;
     private Board previousBoard;
+    
+    private Random generator;
+    
+    private Steal steal = new Steal(12);
+    private FinishOwnStore finishOwnStore = new FinishOwnStore(12);
+    private NoOpp noOpp = new NoOpp(12);
+    private MinOpp minOpp = new MinOpp(12);
+    
+    private ArrayList<Move> moveList = new ArrayList<Move>();
+    
+    public AIPlayer2()
+    {
+        moveList.add(finishOwnStore);
+        moveList.add(steal);
+        moveList.add(noOpp);
+        moveList.add(minOpp);     
+        
+        generator = new Random();
+        
+    }
 
     //private Board.Side P2SideC = currentBoard.getSide(this);
     //private Board.Side P1SideC = currentBoard.getSide(other);
@@ -14,6 +37,10 @@ public class AIPlayer2 extends Player {
     //private Board.Side P1Side = getSide(other);
     
     private int lastMove; //Contains the number of the last move 1,2,3,4
+
+    public void gameFinished(int i)
+    {
+    }
 
     @Override
     public int getSowIndex(Board board, int lastMove) {
@@ -29,33 +56,72 @@ public class AIPlayer2 extends Player {
             //If a type of move succedes then add to value
             //If it fails then reduce value
         
+        //if random number is less than value in first position
+        //if random number is less than runningTotal + value in second position
+        //if random number is less than runningTotal + value in third position
+        //if random number is less than runningTotal + value in forth position
         
-        gameCount++;
-        previousBoard = currentBoard.getBoardForMove(index);
+        int totalValue = 0;
         
-        return index;
+        for(int i = 0; i < moveList.size(); i++)
+        {
+            totalValue = totalValue + moveList.get(i).getValue();
+        }
+        
+        int runningTotal = 0;
+        
+        int randomChoice = generator.nextInt(totalValue);
+        
+        for(int i = 0; i < moveList.size(); i++)
+        {
+            if(runningTotal + moveList.get(i).getValue() > randomChoice)
+            {
+                if(moveList.get(i).run() != -1)
+                {
+                
+                    moveList.incValue(currentBoard.getBoardForMove(moveList.get(i).run()).p2side.getStore() - currentBoard.p2side.getStore());
+                
+                    return moveList.get(i).run();
+                }
+            }
+            
+            runningTotal = runningTotal + moveList.get(i).getValue();
+        }
+        
+        return minOpp.run();
+        
+        //gameCount++;
+        //previousBoard = currentBoard.getBoardForMove(index);
+        
     }
+    
+    private int incStoreGivenMove(int index)
+    {
+        return currentBoard.getBoardForMove(index).p2side.getStore() - currentBoard.p2side.getStore();
+    }
+    
        
     private boolean ourLastMove()
     {
         return currentBoard == previousBoard;
     }
-    
+      
     private int countIncreaseSideOpp(Board predicted)
     {
         int total = 0;
-    
+        
         for(int i = 0; i < NUM_HOUSES;i++)
         {
             total += predicted.getSide(other).getHouse(i) - currentBoard.getSide(other).getHouse(i);
         }
         return total;
-    }
-    
+    }  
+      
+        
     /**
 * A generic class for a move
 */
-    private class Move
+    private class Move extends AIPlayer2
     {
         private int value;
         
@@ -79,15 +145,15 @@ public class AIPlayer2 extends Player {
             value -= dec;
         }
         
-        public int getValue(int vlaue)
+        public int getValue()
         {
             return value;
-        }
+        }      
     }
     
-    private class steal extends Move
+    private class Steal extends Move
     {
-        public steal(int value)
+        public Steal(int value)
         {
             super(value);
         }
@@ -113,9 +179,9 @@ public class AIPlayer2 extends Player {
         }
     }
     
-    private class finishOwnStore extends Move
+    private class FinishOwnStore extends Move
     {
-        public finishOwnStore(int value)
+        public FinishOwnStore(int value)
         {
             super(value);
         }
@@ -139,9 +205,9 @@ public class AIPlayer2 extends Player {
     }
     
     
-    private class noOpp extends Move
+    private class NoOpp extends Move
     {
-        public noOpp(int value)
+        public NoOpp(int value)
         {
             super(value);
         }
@@ -161,9 +227,9 @@ public class AIPlayer2 extends Player {
         }
     }
     
-    private class minOpp extends Move
+    private class MinOpp extends Move
     {
-        public minOpp(int value)
+        public MinOpp(int value)
         {
             super(value);
         }
