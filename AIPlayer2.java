371bbd7ce11f1321b5d89b6859778809124c0989
Joshiez <jhuburn@gmail.com>
2013-12-10 19:36:32 +0000

    Update AIPlayer2.java

--- AIPlayer2.java
+++ AIPlayer2.java
@@ -1,257 +1,204 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class AIPlayer2 extends Player {
+public class AIPlayer1 extends Player {
     private static final int NUM_HOUSES = Kalah.NUM_HOUSES;
-
-    private int gameCount = 0;        
-
-    private int index;
+    
     private Board currentBoard;
     private Board previousBoard;
-    
     private Random generator;
     
-    private Steal steal = new Steal(12);
-    private FinishOwnStore finishOwnStore = new FinishOwnStore(12);
-    private NoOpp noOpp = new NoOpp(12);
-    private MinOpp minOpp = new MinOpp(12);
-    
-    private ArrayList<Move> moveList = new ArrayList<Move>();
-    
-    public AIPlayer2()
-    {
-        moveList.add(finishOwnStore);
-        moveList.add(steal);
-        moveList.add(noOpp);
-        moveList.add(minOpp);     
-        
+    public AIPlayer1()
+    {     
         generator = new Random();
-        
     }
 
-    //private Board.Side P2SideC = currentBoard.getSide(this);
-    //private Board.Side P1SideC = currentBoard.getSide(other);
-    
-    //private Board.Side P2Side = getSide(this);
-    //private Board.Side P1Side = getSide(other);
-    
-    private int lastMove; //Contains the number of the last move 1,2,3,4
-
+    //Here because fuck Bertie
     public void gameFinished(int i)
     {
     }
 
     @Override
-    public int getSowIndex(Board board, int lastMove) {
-    
+    public int getSowIndex(Board board, int lastMove) 
+    {
         this.currentBoard = board;
-    
-        //Remembering previous after chosen move and compare the new board
-            //If same we have two moves in a row
-    
-            //need a game count :)
-            //list of moves (steal,finish in own store, Not put seeds into opp houses, put least into opp houses)
-            //Each move starts a set value
-            //If a type of move succedes then add to value
-            //If it fails then reduce value
-        
-        //if random number is less than value in first position
-        //if random number is less than runningTotal + value in second position
-        //if random number is less than runningTotal + value in third position
-        //if random number is less than runningTotal + value in forth position
-        
-        int totalValue = 0;
-        
-        for(int i = 0; i < moveList.size(); i++)
+        if(test(0))
         {
-            totalValue = totalValue + moveList.get(i).getValue();
+            System.out.println(moveString(0));
+            return run(0);    
         }
-        
-        int runningTotal = 0;
-        
-        int randomChoice = generator.nextInt(totalValue);
-        
-        for(int i = 0; i < moveList.size(); i++)
+        else if(test(1))
         {
-            if(runningTotal + moveList.get(i).getValue() > randomChoice)
+            System.out.println(moveString(1));
+            return run(1);
+        }
+        else
+        {
+            System.out.println(moveString(2));
+            return run(2);
+        }
+    }
+    
+    //Tests whether a move can be carried out
+    private boolean test(int move)
+    {
+        //System.out.println("TESTING MOVE " + move);
+        if(run(move) == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    //Creates a test string for finding out which move is being used
+    private String moveString(int move)
+    {
+        return "Running Move " + move;
+    }
+    
+    //Checks the increase we give to an opponent from sowing from i
+    public int increase(int i)
+    {              
+        int increase = getHouseOwn(i) - (NUM_HOUSES - i);
+        //System.out.println("NUMBER IN HOUSE IS " + currentBoard.getSide(this).getHouse(i));
+        
+        if(increase >= 0)
+        {
+            return increase;
+        }
+        else
+        {
+        return 0;
+        }
+    }
+
+    //Returns true if house is empty    
+    private boolean checkHouseNotEmpty(int i)
+    {   
+        if(currentBoard.getSide(this).getHouse(i) > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    //Returns the number of seeds in a house
+    private int getHouseOwn(int i)
+    {
+        return currentBoard.getSide(this).getHouse(i);
+    }
+    
+    //works out if a move will finish in our own store
+    private boolean willFinishInStore(int i)
+    {   
+        //Works out the distance from the store in a house
+        int distance = NUM_HOUSES - i;
+        
+        if(((getHouseOwn(i) - distance) % 13) == 0) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+   
+    //Runs a sow method based on an int   
+    public int run(int index)
+    {
+        //FINISH IN STORE
+        if(index == 1)
+        {
+            for(int i = 0; i < NUM_HOUSES; i++)
             {
-                if(moveList.get(i).run() != -1)
+                if(willFinishInStore(i))
                 {
-                
-                    moveList.incValue(currentBoard.getBoardForMove(moveList.get(i).run()).p2side.getStore() - currentBoard.p2side.getStore());
-                
-                    return moveList.get(i).run();
+                    //System.out.println("SOWING FROM " + i);
+                    return i;
                 }
             }
             
-            runningTotal = runningTotal + moveList.get(i).getValue();
+            return -1;
         }
         
-        return minOpp.run();
-        
-        //gameCount++;
-        //previousBoard = currentBoard.getBoardForMove(index);
-        
-    }
-    
-    private int incStoreGivenMove(int index)
-    {
-        return currentBoard.getBoardForMove(index).p2side.getStore() - currentBoard.p2side.getStore();
-    }
-    
-       
-    private boolean ourLastMove()
-    {
-        return currentBoard == previousBoard;
-    }
-      
-    private int countIncreaseSideOpp(Board predicted)
-    {
-        int total = 0;
-        
-        for(int i = 0; i < NUM_HOUSES;i++)
+        //STEAL
+        else if(index == 0)
         {
-            total += predicted.getSide(other).getHouse(i) - currentBoard.getSide(other).getHouse(i);
-        }
-        return total;
-    }  
-      
-        
-    /**
-* A generic class for a move
-*/
-    private class Move extends AIPlayer2
-    {
-        private int value;
-        
-        public Move(int value)
-        {
-            this.value = value;
-        }
-        
-        public void setValue(int value)
-        {
-            this.value = value;
-        }
-        
-        public void incValue(int inc)
-        {
-            value += inc;
-        }
-        
-        public void decValue(int dec)
-        {
-            value -= dec;
-        }
-        
-        public int getValue()
-        {
-            return value;
-        }      
-    }
-    
-    private class Steal extends Move
-    {
-        public Steal(int value)
-        {
-            super(value);
-        }
-        
-        public int run()
-        {
-            //Check if opp has empty store, if no the return -1
+            //Check if we have empty store or a store with 13, if no the return -1
             for(int i = 0; i < NUM_HOUSES; i++)
             {
-                if(currentBoard.p2side.getHouse(i) == 0)
+                if((getHouseOwn(i) == 13))
                 {
-                    //Sow from each house, checking if finish in empty house
+                   return i;
+                }
+                else if(getHouseOwn(i) == 0)
+                {
                     for(int j = 0; j < NUM_HOUSES; j++)
                     {
-                        if((currentBoard.getBoardForMove(j).p2side.getHouse(i)) == 1)
+                        if(j > i)
                         {
-                            return j;
+                            if(( (j + getHouseOwn(i)) - i) == 13)
+                            {
+                                return j;
+                            }
+                        }
+                        else if(j < i)
+                        {
+                            if(( (j + getHouseOwn(i)) - i) == 0)
+                            {
+                                return j;
+                            }
                         }
                     }
                 }
             }
             return -1;
-        }
-    }
-    
-    private class FinishOwnStore extends Move
-    {
-        public FinishOwnStore(int value)
+        }      
+
+        //MINOPP
+        else if(index == 2)
         {
-            super(value);
-        }
-        
-        public int run()
-        {
-            for(int i = 0;i < NUM_HOUSES;i++)
-            {
-                Board newBoard = currentBoard.getBoardForMove(i);
-            
-                int ownStoreInc = (newBoard.p2side.getStore()) - (currentBoard.p2side.getStore());
-                int oppHouseInc = (currentBoard.getBoardForMove(i).getSide(other).getHouse(0)) - (currentBoard.getSide(other).getHouse(0));
+            //System.out.println("RUNNING MINOPP");
                 
-                if(ownStoreInc > oppHouseInc)
-                {
-                    return i;
-                }
-            }           
-            return -1;
-        }
-    }
-    
-    
-    private class NoOpp extends Move
-    {
-        public NoOpp(int value)
-        {
-            super(value);
-        }
-        
-        public int run()
-        {
-            int count = 6;
-            
-            for(int i = 0; i < NUM_HOUSES; i++)
-            {
-                if (currentBoard.p2side.getHouse(i) <= count)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-    }
-    
-    private class MinOpp extends Move
-    {
-        public MinOpp(int value)
-        {
-            super(value);
-        }
-        
-        public int run()
-        {
             int min = 49;
             int minIndex = 0;
-        
+            
             for(int i = 0; i < NUM_HOUSES; i++)
             {
-                int count = countIncreaseSideOpp(currentBoard.getBoardForMove(i));
+                int increase = increase(i);
+                //System.out.println("INCREASE IS " + increase);
                 
-                if(count < min)
+                if((increase >= 0) && (increase < min) && checkHouseNotEmpty(i))
                 {
-                    min = count;
+                    min = increase;
+                    //System.out.println("MIN IS " + min);
                     minIndex = i;
                 }
             }
-            
+            //System.out.println("SOWING FROM " + minIndex);
             return minIndex;
-            
         }
-    }   
+        
+        return generator.nextInt(5);   
+        
+     }
 }
+                    
+                    
+                
+    
+    
+    
+    
+    
+    
+    
+    
+    
