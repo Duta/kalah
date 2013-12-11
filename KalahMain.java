24ec470949c3917b9602f29785df005085d66d79
Bertie Wheen <wheen.b@gmail.com>
2013-12-11 15:32:02 +0000

    Finalization

--- KalahMain.java
+++ KalahMain.java
@@ -3,17 +3,28 @@ public class KalahMain {
         int numP1wins = 0;
         int numP2wins = 0;
         int numDraws = 0;
+        int totalP1store = 0;
+        int totalP2store = 0;
         for(int i = 0; i < 1000; i++) {
             Kalah kalah = new Kalah(i % 2 == 0);
-            int winner = kalah.run();
-            if(winner == 1) {
+            GameResult result = kalah.run();
+            Winner winner = result.getWinner();
+            if(winner == Winner.PLAYER_ONE) {
                 numP1wins++;
-            } else if(winner == 2) {
+            } else if(winner == Winner.PLAYER_TWO) {
                 numP2wins++;
             } else {
                 numDraws++;
             }
-            printState(numP1wins, numP2wins, numDraws, i + 1);
+            totalP1store += result.getP1store();
+            totalP2store += result.getP2store();
+            printState(
+                numP1wins,
+                numP2wins,
+                numDraws,
+                totalP1store,
+                totalP2store,
+                i + 1);
         }
         String victor;
         if(numP1wins > numP2wins) {
@@ -27,38 +38,17 @@ public class KalahMain {
         System.out.println("P2 wins: " + numP2wins);
         System.out.println("Draws:   " + numDraws);
         System.out.println("Victor:  " + victor);
+        System.out.println("P1 store total: " + totalP1store);
+        System.out.println("P2 store total: " + totalP2store);
     }
 
-    private static void printState(int numP1wins, int numP2wins, int numDraws, int numGames) {
+    private static void printState(int numP1wins, int numP2wins, int numDraws,
+            int totalP1store, int totalP2store, int numGames) {
         double p1pct = (100.0*numP1wins)/numGames;
         double p2pct = (100.0*numP2wins)/numGames;
         double drpct = (100.0*numDraws)/numGames;
         System.out.printf(
-            "P1: %7.3f%% | P2: %7.3f%% | Draw: %7.3f%% | No. Games: %4d\n",
-            p1pct, p2pct, drpct, numGames);
-    }
-
-    private static void printStateOld(int numP1wins, int numP2wins, int numDraws, int numGames) {
-        int width = 10;
-        double factor = (double)width/numGames;
-        int a = (int)Math.round(numP1wins * factor);
-        int b = (int)Math.round(numP2wins * factor);
-        int c = (int)Math.round(numDraws * factor);
-        StringBuilder sb = new StringBuilder();
-        sb.append('|');
-        for(int i = 0; i < a; i++) {
-            sb.append('1');
-        }
-        sb.append('|');
-        for(int i = 0; i < b; i++) {
-            sb.append('2');
-        }
-        sb.append('|');
-        for(int i = 0; i < c; i++) {
-            sb.append('d');
-        }
-        sb.append("| Num Games: ");
-        sb.append(numGames);
-        System.out.println(sb.toString());
+            "P1: %7.3f%% | P2: %7.3f%% | Draw: %7.3f%% | Total P1 store: %5d | Total P2 store: %5d | No. Games: %4d\n",
+            p1pct, p2pct, drpct, totalP1store, totalP2store, numGames);
     }
 }
